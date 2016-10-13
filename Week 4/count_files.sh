1b0cd31bfdf8ad5f54627ea9ba529c370f0eadea
John Burns <jzburns@gmail.com>
2016-10-13 10:56:27 +0100

    allows user input dir now

--- Week 4/count_files.sh
+++ Week 4/count_files.sh
@@ -3,5 +3,6 @@
 #find will bring you to the path you specified in the path
 #-type f will specify that it is looking for a file 
 # wc is a word count command and coupled with -l will count all words(files) in the directory 
-#all together it will go to the bin dir look only at the contents of type file and count them
-find /bin -type f | wc -l
+#all together it will go to the user selected dir look only at the contents of type file and count them
+echo "scanning $1 directory"
+find $1 -type f | wc -l
